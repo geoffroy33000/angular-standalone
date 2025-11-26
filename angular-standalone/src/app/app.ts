dccf8208fd3227786a921998408922223feb8b7c
Geoffroy3 <geoffroy.pailhebelair@gmail.com>
2025-11-26 17:03:21 +0100

    change app content

--- angular-standalone/src/app/app.ts
+++ angular-standalone/src/app/app.ts
@@ -1,12 +1,12 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, signal} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('angular-standalone');
+export class AppComponent {
+  title = 'angular-standalone';
+  paragraph = 'lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat';
 }
